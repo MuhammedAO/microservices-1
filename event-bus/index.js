594d8859200775461e377a2effaf0600a49b396e
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-05-27 09:08:30 +0100

    basic event bus

---
+++ event-bus/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const axios = require("axios")
+
+const app = express()
+app.use(express.json())
+
+app.post("/events", (req, res) => {
+  const event = req.body
+
+  axios.post("http://localhost:4000/events", event)
+  axios.post("http://localhost:5000/events", event)
+  axios.post("http://localhost:6000/events", event)
+    
+  res.send({ status: "Ok" })
+})
+
+app.listen(7000, () => console.log("Listening on 7000"))
