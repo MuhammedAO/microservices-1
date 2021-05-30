4225de234824daf05dd4ace1dab08a9a1a2e4168
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-05-30 22:03:57 +0100

    Moderation service

---
+++ moderation/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const axios = require("axios")
+
+const app = express()
+app.use(express.json())
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved"
+
+    await axios.post("http://localhost:7000/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    })
+  }
+
+  res.send({})
+})
+
+app.listen(8001, () => {
+  console.log("Listening on 8001")
+})
