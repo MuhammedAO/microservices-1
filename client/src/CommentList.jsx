8392f70bb9d2f21d6e0bc373aeb54b72f91913c8
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-05-31 21:25:03 +0100

    Render comments by status

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,7 +1,7 @@
-import React,{useState, useEffect} from "react"
-import axios from 'axios'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-const CommentList = ({comments}) => {
+const CommentList = ({ comments }) => {
   // const [comments, setComments] = useState([])
 
   // const fetchComments = async () => {
@@ -15,17 +15,25 @@ const CommentList = ({comments}) => {
   //   fetchComments()
   // }, [])
 
-  const renderedComments = comments.map(comment => {
-    return (
-      <li key={comment.id}>{comment.content}</li>
-    )
+  const renderedComments = comments.map((comment) => {
+    let content
+
+    if (comment.status === "approved") {
+      content = comment.content
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation"
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected"
+    }
+
+    return <li key={comment.id}>{content}</li>
   })
 
-  return (
-  <ul>
-  {renderedComments}
-  </ul>
-  )
+  return <ul>{renderedComments}</ul>
 }
 
 export default CommentList
