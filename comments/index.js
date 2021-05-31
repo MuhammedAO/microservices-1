2a76e52970750ef2be754548e6f6c3f2f76d2ba4
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-05-31 20:15:21 +0100

    Updating comment content

--- comments/index.js
+++ comments/index.js
@@ -24,7 +24,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   comments.push({
     id: commentId,
     content,
-    status:'pending'
+    status: "pending",
   })
 
   commentByPostId[req.params.id] = comments
@@ -34,16 +34,40 @@ app.post("/posts/:id/comments", async (req, res) => {
     data: {
       id: commentId,
       content,
-			postId: req.params.id,
-      status:'pending'
+      postId: req.params.id,
+      status: "pending",
     },
   })
 
   res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
-  console.log('Recieved Event:', req.body.type)
+app.post("/events", async (req, res) => {
+  console.log("Recieved Event:", req.body.type)
+
+  const { type, data } = req.body
+
+  if (type === "CommentModerated") {
+    const { id, postId, status, content } = data
+
+    const comments = commentByPostId[postId]
+
+    const comment = comments.find((comment) => {
+      return (comment.id = id)
+    })
+
+    comment.status = status
+
+    await axios.post("http://localhost:7000/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    })
+  }
 
   res.send({})
 })
