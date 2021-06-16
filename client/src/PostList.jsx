132f607a7b444474c46a2262e7bc7ba5ccf981f9
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-16 12:55:46 +0100

    deploying react app

--- client/src/PostList.jsx
+++ client/src/PostList.jsx
@@ -8,7 +8,7 @@ const PostList = () => {
   const [posts, setPosts] = useState({})
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:8000/posts')
+    const res = await axios.get('http://posts.com/posts')
     setPosts(res.data)
 
   }
